c594a3bf080e995e69f5744df5247347270c01c9
xkz7y7hpbh-bit <xkz7y7hpbh@privaterelay.appleid.com>
2026-05-01 22:46:14 +0300

    Create Aside.js

---
+++ components/Aside.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Aside.css';
+
+function Aside() {
+  return (
+    <aside className="aside">
+      <p>Иванов Иван Иванович</p>
+    </aside>
+  );
+}
+export default Aside;
